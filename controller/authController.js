977c21213e43119ece9e6814dce521459613bef8
LuizFernandoBMoragas <luiz.moragas@hotmail.com>
2024-04-22 00:47:52 +0300

    authentication route and controller added, V1

---
+++ controller/authController.js
@@ -0,0 +1,10 @@
+const signup = (req, res, next) =>{
+    res.json({
+        status: "Raboti",
+        message: "sign up will work here"
+    });
+};
+
+module.exports = { signup };
+
+
--- server.js
+++ server.js
@@ -1,14 +1,29 @@
+require('dotenv').config({ path: `${process.cwd}/env` });
+
 const express = require('express');
+
+const authRouter = require('./route/authRoute.js');
 
 const app = express();
 
 app.get('/', (req, res)=>{
     res.status(200).json({
-        "status": "Working",
-        "message": "Good, server is up and running"
+        status: "Working",
+        Message: "Wohoo, the server is up and running"
     })
 });
 
-app.listen(3333, ()=>{
-    console.log('Server is up and running')
+app.use('/api/v1/auth', authRouter)
+
+app.use('*', (req, res)=>{
+    res.status(404).json({
+        status: "Fail",
+        message: "Route not found"
+    })
+})
+
+const PORT = process.env.APP_PORT || 3333;
+
+app.listen(PORT, ()=>{
+    console.log('Server is up and running!', PORT);
 })
